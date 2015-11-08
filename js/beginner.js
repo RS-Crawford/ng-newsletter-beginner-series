168ef32f5f2d71c940ba18b06a7ab651284acaf1
Robert S Crawford <craw.rs@gmail.com>
2015-11-08 11:48:56 -0800

    Committing for push

--- js/beginner.js
+++ js/beginner.js
@@ -1,14 +1,30 @@
 var app = angular.module('myApp', []),
-    apiKey = 'MDExODQ2OTg4MDEzNzQ5OTM4Nzg5MzFiZA001',
+    apiKey = 'MDIxMTYzOTE4MDE0NDY4NDg0NjM2ZWY0MQ000',
     nprUrl = 'http://api.npr.org/query?id=61&fields=relatedLink,title,byline,text,audio,image,pullQuote,all&output=JSON';
 
 
 app.controller('PlayerController', ['$scope', '$http', function($scope, $http) {
-  var audio = document.createElement('audio');
-  $scope.audio = audio;
+  $scope.playing = false;
+  $scope.audio = document.createElement('audio');
 
-  audio.src = 'http://pd.npr.org/npr-mp4/npr/sf/2013/07/20130726_sf_05.mp4?orgId=1&topicId=1032&ft=3&f=61';
-  audio.play();
+  $scope.audio.src = 'http://pd.npr.org/npr-mp4/npr/sf/2013/07/20130726_sf_05.mp4?orgId=1&topicId=1032&ft=3&f=61';
+  //audio.play();
+
+  $scope.play = function () {
+    $scope.audio.play();
+    scope.playing = true;
+  };
+
+  $scope.stop = function () {
+    $scope.audio.stop();
+    scope.playing = false;
+  }
+
+  $scope.audio.addEventListener('ended', function() {
+    $scope.$apply(function() {
+      $scope.stop()
+    })
+  })
 
   $http({
     method: 'JSONP',
